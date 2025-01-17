1d9f59f3487e44070608b2fa4f7f0f0bcebf8e22
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-01-17 21:59:55 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState,useEffect } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+
+function App() {
+  const [todos,setTodos]=useState([
+  ])
+  const [todoValue,setTodosValue]=useState('')
+
+  function persistData(newList){
+    localStorage.setItem('todos',JSON.stringify({todos:
+      newList
+    }))
+  }
+  function handleAddTodos(newTodos){
+    const newTodoList=[...todos,newTodos]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+  function handleDeleteTodos(index){
+    const newTodoList=todos.filter((todos,todoIndex)=>{
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+  function handleEditTodos(index){
+    const valueToBeEdited=todos[index]
+    setTodosValue(valueToBeEdited)
+    handleDeleteTodos(index)
+  }
+
+  useEffect(()=>{
+    if(!localStorage){
+      return
+    }
+    let localTodos=localStorage.getItem('todos')
+    if(!localTodos){
+      return
+    }
+    localTodos=JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  },[])
+  return (
+    <main>
+      <TodoInput todoValue={todoValue} setTodosValue={setTodosValue} handleAddTodos={handleAddTodos}/>
+      <TodoList handleEditTodos={handleEditTodos} handleDeleteTodos={handleDeleteTodos} todos={todos}/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function TodoInput(props){
+    const {handleAddTodos,todoValue,setTodosValue}=props
+    return(
+        <header>
+            <input value={todoValue}  onChange={(e)=>{
+                setTodosValue(e.target.value)   
+            }} placeholder="Enter todo...."/>
+            <button onClick={() => {
+                handleAddTodos(todoValue)
+                setTodosValue('')
+                }}>Add</button>
+        </header>
+    )
+}
